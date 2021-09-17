0f986e5e6da9848bbd806ff2d96b72a758d896eb
Pranay Valson <pranay.valson@gmail.com>
2021-09-17 09:55:41 -0700

    updates proof package for complete payload proof txs

--- internal/proof/proof.go
+++ internal/proof/proof.go
@@ -2,25 +2,35 @@ package proof
 
 import (
 	"context"
-	"fmt"
+	"crypto/sha256"
+	"encoding/json"
 	"math/big"
+	"time"
 
 	"github.com/covalenthq/mq-store-agent/internal/config"
 	"github.com/covalenthq/mq-store-agent/internal/event"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
 )
 
-func SubmitSpecimenProofTx(config *config.Config, chainHeight uint64, blockSpecimen event.SpecimenEvent) {
+var (
+	proofTxTimeout uint64 = 60
+	chainLen       uint64 = 1
+)
+
+func SubmitSpecimenProofTx(config *config.Config, chainHeight uint64, blockSpecimen event.SpecimenEvent, txHash chan string) {
 
 	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*time.Duration(proofTxTimeout))
+	defer cancel()
 	//var onlyOnce sync.Once
 
-	sendAddr, opts, chainId, err := GetTransactionOpts(config)
+	_, opts, chainId, err := GetTransactionOpts(config)
 	if err != nil {
 		log.Error("error in getting transaction ops: %v", err)
 	}
@@ -30,74 +40,57 @@ func SubmitSpecimenProofTx(config *config.Config, chainHeight uint64, blockSpeci
 		log.Error("error in getting prover eth client: ", err.Error())
 	}
 
-	number, err := ethclient.BlockNumber(ctx)
-	if err != nil {
-		log.Error("error in getting block number: %v", err)
-	}
-	fmt.Println(ethclient.BalanceAt(ctx, sendAddr, new(big.Int).SetUint64(number)))
-
 	contractAddress := common.HexToAddress(config.EthConfig.Contract)
 	contract, err := NewProofChain(contractAddress, ethclient)
+
 	if err != nil {
 		log.Error("error in binding to deployed contract: %v", err)
 	}
-
-	fmt.Println("specimen:", blockSpecimen.ReplicationEvent.Hash, "sender:", sendAddr, "receiver:", common.HexToAddress(config.EthConfig.Contract), "chainid:", chainId)
 
 	// onlyOnce.Do(func() {
 	// 	WatchContractResultPublicationProof(contract)
 	// })
 
-	// jsonSpecimen, err := json.Marshal(blockSpecimen)
-	// if err != nil {
-	// 	log.Error(err.Error())
-	// }
-	//sha256Specimen := sha256.Sum256(jsonSpecimen)
+	jsonSpecimen, err := json.Marshal(blockSpecimen)
+	if err != nil {
+		log.Error(err.Error())
+	}
+	sha256Specimen := sha256.Sum256(jsonSpecimen)
 
-	signedTx, err := contract.ProveBlockSpecimenProduced(opts, uint64(chainId), chainHeight)
+	tx, err := contract.ProveBlockSpecimenProduced(opts, uint64(chainId), chainHeight, chainLen, uint64(len(jsonSpecimen)), sha256Specimen)
 	if err != nil {
 		log.Error("error in calling deployed contract: %v", err)
 	}
 
-	fmt.Println(signedTx)
+	receipt, err := bind.WaitMined(ctx, ethclient, tx)
+	if receipt.Status != types.ReceiptStatusSuccessful {
+		log.Error("Block-specimen proof tx call: ", tx.Hash(), " to proof contract failed: ", err)
+	}
+	if err != nil {
+		log.Error(err.Error())
+	}
 
-	// signedTx, err := types.SignTx(tx, types.NewEIP155Signer(nil), secret)
-	// if err != nil {
-	// 	log.Error("error in signing tx: %v", err)
-	// }
+	txHash <- receipt.TxHash.String()
 
-	// receipt, err := bind.WaitMined(ctx, ethclient, signedTx)
-	// if receipt.Status != types.ReceiptStatusSuccessful {
-	// 	panic("Call failed")
-	// 	//log.Error("Specimen proof tx call: %v , to contract failed: %v", signedTx.Hash(), err)
-	// 	//return signedTx.Hash().String(), false, err
-	// }
-	log.Info("Proof of specimen signed by : %v has been sent to mempool at nonce: %v", opts.Signer, opts.Nonce)
-
-	//return signedTx.Hash().String(), true, err
 }
 
-func SubmitResultProofTx(config *config.Config, chainHeight uint64, blockResult event.ResultEvent) {
+func SubmitResultProofTx(config *config.Config, chainHeight uint64, blockResult event.ResultEvent, txHash chan string) {
 
 	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*time.Duration(proofTxTimeout))
+	defer cancel()
 	//var onlyOnce sync.Once
 
-	sendAddr, opts, chainId, err := GetTransactionOpts(config)
+	_, opts, chainId, err := GetTransactionOpts(config)
 	if err != nil {
 		log.Error("error in getting transaction ops: %v", err)
 	}
-
-	fmt.Println("result:", blockResult.ReplicationEvent.Hash, sendAddr, chainId)
 
 	ethclient, err := GetEthClient(config.EthConfig.ProverClient)
 	if err != nil {
 		log.Error("error in getting prover eth client: ", err.Error())
 	}
-	number, err := ethclient.BlockNumber(ctx)
-	if err != nil {
-		log.Error("error in getting block number: %v", err)
-	}
-	fmt.Println(ethclient.BalanceAt(ctx, sendAddr, new(big.Int).SetUint64(number)))
 
 	contractAddress := common.HexToAddress(config.EthConfig.Contract)
 	contract, err := NewProofChain(contractAddress, ethclient)
@@ -109,36 +102,26 @@ func SubmitResultProofTx(config *config.Config, chainHeight uint64, blockResult 
 	// 	WatchContractSpecimenPublicationProof(contract)
 	// })
 
-	fmt.Println("result:", blockResult.ReplicationEvent.Hash, "sender:", sendAddr, "receiver:", common.HexToAddress(config.EthConfig.Contract), "chainid:", chainId)
+	jsonResult, err := json.Marshal(blockResult)
+	if err != nil {
+		log.Error(err.Error())
+	}
+	sha256Result := sha256.Sum256(jsonResult)
 
-	// jsonResult, err := json.Marshal(blockResult)
-	// if err != nil {
-	// 	log.Error(err.Error())
-	// }
-	// sha256Result := sha256.Sum256(jsonResult)
-
-	signedTx, err := contract.ProveBlockSpecimenProduced(opts, uint64(chainId), chainHeight)
+	tx, err := contract.ProveBlockSpecimenProduced(opts, uint64(chainId), chainHeight, chainLen, uint64(len(jsonResult)), sha256Result)
 	if err != nil {
 		log.Error("error in calling deployed contract: %v", err)
 	}
 
-	fmt.Println(signedTx)
+	receipt, err := bind.WaitMined(ctx, ethclient, tx)
+	if receipt.Status != types.ReceiptStatusSuccessful {
+		log.Error("Block-result proof tx call: ", tx.Hash(), " to proof contract failed: ", err)
+	}
+	if err != nil {
+		log.Error(err.Error())
+	}
 
-	// signedTx, err := types.SignTx(tx, types.NewEIP155Signer(nil), secret)
-	// if err != nil {
-	// 	log.Error("error in signing tx: %v", err)
-	// }
-
-	// receipt, err := bind.WaitMined(ctx, ethclient, signedTx)
-	// if receipt.Status != types.ReceiptStatusSuccessful {
-	// 	panic("Call failed")
-	// 	//log.Error("Result proof tx call: %v , to contract failed: %v", signedTx.Hash(), err)
-	// 	//return signedTx.Hash().String(), false, err
-	// }
-
-	log.Info("Proof of result tx signed by : %v has been sent to mempool at nonce: %v", opts.Signer, opts.Nonce)
-
-	//return signedTx.Hash().String(), true, err
+	txHash <- receipt.TxHash.String()
 }
 
 func GetEthClient(address string) (*ethclient.Client, error) {
@@ -181,42 +164,43 @@ func GetKeyStore(config *config.Config) (*bind.TransactOpts, error) {
 	return ksOpts, err
 }
 
-// func WatchContractResultPublicationProof(contract *ProofChain) {
-// 	ctx := context.Background()
-// 	// Watch for a Deposited event
-// 	watchOpts := &bind.WatchOpts{Context: ctx, Start: nil}
-// 	// Setup a channel for results
-// 	channel := make(chan *ProofChainBlockResultPublicationProofAppended)
-// 	// Start a goroutine which watches new events
-// 	go func() {
-// 		sub, err := contract.WatchBlockResultPublicationProofAppended(watchOpts, channel)
-// 		if err != nil {
-// 			log.Error("error in watching contract for result proof event: ", err.Error())
-// 		}
-// 		defer sub.Unsubscribe()
-// 	}()
-// 	// Receive events from the channel
-// 	event := <-channel
+func WatchContractResultPublicationProof(contract *ProofChain) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*time.Duration(proofTxTimeout))
+	defer cancel()
 
-// 	log.Info("new result event emitted from prover contract: %v", event)
-// }
+	watchOpts := &bind.WatchOpts{Context: ctx, Start: nil}
+	channel := make(chan *ProofChainBlockResultPublicationProofAppended)
 
-// func WatchContractSpecimenPublicationProof(contract *ProofChain) {
-// 	ctx := context.Background()
-// 	// Watch for a Deposited event
-// 	watchOpts := &bind.WatchOpts{Context: ctx, Start: nil}
-// 	// Setup a channel for results
-// 	channel := make(chan *ProofChainBlockSpecimenPublicationProofAppended)
-// 	// Start a goroutine which watches new events
-// 	go func() {
-// 		sub, err := contract.WatchBlockSpecimenPublicationProofAppended(watchOpts, channel)
-// 		if err != nil {
-// 			log.Error("error in watching contract for specimen proof event: ", err.Error())
-// 		}
-// 		defer sub.Unsubscribe()
-// 	}()
-// 	// Receive events from the channel
-// 	event := <-channel
+	go func() {
+		sub, err := contract.WatchBlockResultPublicationProofAppended(watchOpts, channel)
+		if err != nil {
+			log.Error("error in watching contract for result proof event: ", err.Error())
+		}
+		defer sub.Unsubscribe()
+	}()
 
-// 	log.Info("new specimen event emitted from prover contract: %v", event)
-// }
+	event := <-channel
+	log.Info("new result event emitted from prover contract: %v", event)
+
+}
+
+func WatchContractSpecimenPublicationProof(contract *ProofChain) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*time.Duration(proofTxTimeout))
+	defer cancel()
+
+	watchOpts := &bind.WatchOpts{Context: ctx, Start: nil}
+	channel := make(chan *ProofChainBlockSpecimenPublicationProofAppended)
+
+	go func() {
+		sub, err := contract.WatchBlockSpecimenPublicationProofAppended(watchOpts, channel)
+		if err != nil {
+			log.Error("error in watching contract for specimen proof event: ", err.Error())
+		}
+		defer sub.Unsubscribe()
+	}()
+
+	event := <-channel
+	log.Info("new specimen event emitted from prover contract: %v", event)
+}
