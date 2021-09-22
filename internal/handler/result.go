8594285462efd509c1d5bc22b26f8140395403a6
Pranay Valson <pranay.valson@gmail.com>
2021-09-22 16:06:15 -0700

    corrects block-result avro schema that encodes fully

--- internal/handler/result.go
+++ internal/handler/result.go
@@ -9,6 +9,7 @@ import (
 	"cloud.google.com/go/storage"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/linkedin/goavro/v2"
 	log "github.com/sirupsen/logrus"
 	"github.com/ubiq/go-ubiq/rlp"
 
@@ -17,6 +18,7 @@ import (
 	"github.com/covalenthq/mq-store-agent/internal/proof"
 	st "github.com/covalenthq/mq-store-agent/internal/storage"
 	"github.com/covalenthq/mq-store-agent/internal/types"
+	"github.com/covalenthq/mq-store-agent/internal/utils"
 )
 
 type resultHandler struct {
@@ -56,6 +58,8 @@ func (h *resultHandler) Handle(config *config.Config, storage *storage.Client, e
 		log.Error("error in getting block: ", err.Error())
 	}
 
+	EncodeResultToAvro(result)
+
 	log.Info("Submitting block-result proof for: ", block.Number.Uint64())
 
 	proofTxHash := make(chan string, 1)
@@ -78,4 +82,305 @@ func (h *resultHandler) Handle(config *config.Config, storage *storage.Client, e
 	}
 
 	return nil
+}
+
+func EncodeResultToAvro(blockResult interface{}) {
+	codec, err := goavro.NewCodec(`
+	{
+		"type": "record",
+		"name": "BlockResult",
+		"namespace": "com.covalenthq.blockresult.avro",
+		"fields": [
+		  {
+			"name": "ReplicationEvent",
+			"type": {
+			  "name": "ReplicationEvent",
+			  "type": "record",
+			  "fields": [
+				{
+				  "name": "ID",
+				  "type": "string"
+				},
+				{
+				  "name": "type",
+				  "type": "string"
+				},
+				{
+				  "name": "hash",
+				  "type": "string"
+				},
+				{
+				  "name": "datetime",
+				  "type": "string"
+				}
+			  ]
+			}
+		  },
+		  {
+			"name": "result",
+			"type": {
+			  "name": "result",
+			  "type": "record",
+			  "fields": [
+				{
+				  "name": "Hash",
+				  "type": "string"
+				},
+				{
+				  "name": "TotalDifficulty",
+				  "type": "int"
+				},
+				{
+				  "name": "Header",
+				  "type": {
+					"name": "Header",
+					"type": "record",
+					"fields": [
+					  {
+						"name": "parentHash",
+						"type": "string"
+					  },
+					  {
+						"name": "sha3Uncles",
+						"type": "string"
+					  },
+					  {
+						"name": "miner",
+						"type": "string"
+					  },
+					  {
+						"name": "stateRoot",
+						"type": "string"
+					  },
+					  {
+						"name": "transactionsRoot",
+						"type": "string"
+					  },
+					  {
+						"name": "receiptsRoot",
+						"type": "string"
+					  },
+					  {
+						"name": "logsBloom",
+						"type": {
+						  "type": "array",
+						  "items": "int"
+						}
+					  },
+					  {
+						"name": "difficulty",
+						"type": "int"
+					  },
+					  {
+						"name": "number",
+						"type": "int"
+					  },
+					  {
+						"name": "gasLimit",
+						"type": "int"
+					  },
+					  {
+						"name": "gasUsed",
+						"type": "int"
+					  },
+					  {
+						"name": "timestamp",
+						"type": "int"
+					  },
+					  {
+						"name": "extraData",
+						"type": "string"
+					  },
+					  {
+						"name": "mixHash",
+						"type": "string"
+					  },
+					  {
+						"name": "nonce",
+						"type": {
+						  "type": "array",
+						  "items": "int"
+						}
+					  },
+					  {
+						"name": "baseFeePerGas",
+						"type": "int"
+					  }
+					]
+				  }
+				},
+				{
+				  "name": "Transactions",
+				  "type": {
+					"type": "array",
+					"items": {
+					  "name": "Transactions_record",
+					  "type": "record",
+					  "fields": [
+						{
+						  "name": "nonce",
+						  "type": "int"
+						},
+						{
+						  "name": "gasPrice",
+						  "type": "long"
+						},
+						{
+						  "name": "gas",
+						  "type": "int"
+						},
+						{
+						  "name": "from",
+						  "type": "string"
+						},
+						{
+						  "name": "to",
+						  "type": "string"
+						},
+						{
+						  "name": "value",
+						  "type": "long"
+						},
+						{
+						  "name": "input",
+						  "type": "string"
+						}
+					  ]
+					}
+				  }
+				},
+				{
+					"name": "uncles",
+					"type": [
+						"null", {
+							"type": "array",
+							"items": "Header"
+						}
+					],
+					"default": null
+				},
+				{
+				  "name": "Receipts",
+				  "type": {
+					"type": "array",
+					"items": {
+					  "name": "Receipts_record",
+					  "type": "record",
+					  "fields": [
+						{
+						  "name": "PostStateOrStatus",
+						  "type": "string"
+						},
+						{
+						  "name": "CumulativeGasUsed",
+						  "type": "int"
+						},
+						{
+						  "name": "TxHash",
+						  "type": "string"
+						},
+						{
+						  "name": "ContractAddress",
+						  "type": "string"
+						},
+						{
+						  "name": "Logs",
+						  "type": {
+							"type": "array",
+							"items": {
+							  "name": "Logs_record",
+							  "type": "record",
+							  "fields": [
+								{
+								  "name": "address",
+								  "type": "string"
+								},
+								{
+								  "name": "topics",
+								  "type": {
+									"type": "array",
+									"items": "string"
+								  }
+								},
+								{
+								  "name": "data",
+								  "type": "string"
+								},
+								{
+								  "name": "blockNumber",
+								  "type": "int"
+								},
+								{
+								  "name": "transactionHash",
+								  "type": "string"
+								},
+								{
+								  "name": "transactionIndex",
+								  "type": "int"
+								},
+								{
+								  "name": "blockHash",
+								  "type": "string"
+								},
+								{
+								  "name": "logIndex",
+								  "type": "int"
+								},
+								{
+								  "name": "removed",
+								  "type": "boolean"
+								}
+							  ]
+							}
+						  }
+						},
+						{
+						  "name": "GasUsed",
+						  "type": "int"
+						}
+					  ]
+					}
+				  }
+				},
+				{
+				  "name": "Senders",
+				  "type": {
+					"type": "array",
+					"items": "string"
+				  }
+				}
+			  ]
+			}
+		  }
+		]
+	  }`)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resultMap, err := utils.StructToMap(blockResult)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Convert native Go form to binary Avro data
+	binary, err := codec.BinaryFromNative(nil, resultMap)
+	if err != nil {
+		log.Fatalf("Failed to convert Go map to Avro binary data: %v", err)
+	}
+	_ = binary
+
+	//Convert binary Avro data back to native Go form
+	native, _, err := codec.NativeFromBinary(binary)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//Convert native Go form to textual Avro data
+	textual, err := codec.TextualFromNative(nil, native)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(textual))
 }
