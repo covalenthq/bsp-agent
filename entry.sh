06805d0865ede6236e438991478ebc4b5050119b
Pranay Valson <pranay.valson@gmail.com>
2021-11-29 16:26:38 -0800

    corrects eth client connection flag in entry script

--- entry.sh
+++ entry.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 ./main --redis-url=redis://username:@redis:6379/0?topic=replication#replicate  \
  --codec-path=./codec/block-replica.avsc \
  --binary-file-path=./bin/block-replica/ \
  --gcp-svc-account=/Users/user/.config/gcloud/bsp.json \
  --replica-bucket=covalenthq-geth-block-specimen \
  --segment-length=10 \
- --eth-client=http://127.0.0.1:7545  \
+ --eth-client=http://0.0.0.0:8545  \
  --proof-chain-address=0xEa2ff902dbeEECcc828757B881b343F9316752e5 \
  --consumer-timeout=80
