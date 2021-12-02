e565360312f4d1ca932773b1cbddaf5f3f60b2a9
Pranay Valson <pranay.valson@gmail.com>
2021-12-01 16:44:57 -0800

    removes cqt virtnet dependency related files

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const { deployProxy } = require('@openzeppelin/truffle-upgrades');
-
-const ProofChain = artifacts.require("ProofChain");
-
-module.exports = async function(deployer, network, accounts) {
-  const instance = await deployProxy(ProofChain, [accounts[0]], { deployer });
-  console.log("Deployed", instance.address);
-};
--- migrations/3_upgrade_contracts.js
+++
@@ -1,9 +0,0 @@
-const { upgradeProxy } = require('@openzeppelin/truffle-upgrades');
-
-const ProofChain = artifacts.require("ProofChain");
-
-module.exports = async function(deployer) {
-  const existing = await ProofChain.deployed();
-  const instance = await upgradeProxy(existing.address, ProofChain, { deployer });
-  console.log("Upgraded", instance.address);
-};
