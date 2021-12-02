1ed069df8bc53fe46b64a7038866347eaaf1080e
Pranay Valson <pranay.valson@gmail.com>
2021-12-01 16:30:08 -0800

    pauses main till all contracts deployed on ganache

--- entry.sh
+++ entry.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+sleep 60
+
 ./main --redis-url=redis://username:@redis:6379/0?topic=replication#replicate  \
  --codec-path=./codec/block-replica.avsc \
  --binary-file-path=./bin/block-replica/ \
  --gcp-svc-account=/Users/user/.config/gcloud/bsp.json \
  --replica-bucket=covalenthq-geth-block-specimen \
  --segment-length=10 \
- --eth-client=http://0.0.0.0:8545  \
+ --eth-client=http://ganache-cli:8545  \
  --proof-chain-address=0xEa2ff902dbeEECcc828757B881b343F9316752e5 \
  --consumer-timeout=80
