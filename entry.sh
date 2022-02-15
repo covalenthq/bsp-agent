f24a52778ac4bac91babf8e715aa4a2cae4fc96e
Pranay Valson <pranay.valson@gmail.com>
2022-02-15 12:25:24 -0800

    amends entry.sh for docker ci

--- entry.sh
+++ entry.sh
@@ -4,17 +4,15 @@ then
     timeout 120s ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication#replicate \
     --avro-codec-path=./codec/block-elrond.avsc \
     --binary-file-path=./bin/block-elrond/ \
-    --replica-bucket=covalenthq-geth-block-specimen \
     --segment-length=10 \
     --proof-chain-address=0xEa2ff902dbeEECcc828757B881b343F9316752e5 \
-    --consumer-timeout=60 \
+    --consumer-timeout=15 \
     --websocket-urls="34.66.210.112:20000 34.66.210.112:20001 34.66.210.112:20002 34.66.210.112:20003" 
 else
     ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication#replicate  \
     --avro-codec-path=./codec/block-ethereum.avsc \
     --binary-file-path=./bin/block-ethereum/ \
-    --replica-bucket=covalenthq-geth-block-specimen \
     --segment-length=10 \
     --proof-chain-address=0xEa2ff902dbeEECcc828757B881b343F9316752e5 \
-    --consumer-timeout=60
+    --consumer-timeout=15
 fi
