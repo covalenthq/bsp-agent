6f09f163c35347a020daf91ce6401d6005727aad
Pranay Valson <pranay.valson@gmail.com>
2022-04-04 12:46:32 -0700

    updates redis data dump for rinkeby main data
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$BLOCKCHAIN" == "elrond" ]
 then
-    timeout 120s ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication#replicate \
+    timeout 120s ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-1#replicate \
     --avro-codec-path=./codec/block-elrond.avsc \
     --binary-file-path=./bin/block-elrond/ \
     --segment-length=10 \
@@ -9,7 +9,7 @@ then
     --consumer-timeout=15 \
     --websocket-urls="34.66.210.112:20000 34.66.210.112:20001 34.66.210.112:20002 34.66.210.112:20003" 
 else
-    ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication#replicate  \
+    ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-1#replicate  \
     --avro-codec-path=./codec/block-ethereum.avsc \
     --binary-file-path=./bin/block-ethereum/ \
     --segment-length=10 \
