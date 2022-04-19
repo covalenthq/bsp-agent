c2e0536846d5dc8af10c267f62420324dd9a1231
Pranay Valson <pranay.valson@gmail.com>
2022-04-18 18:14:23 -0700

    exits fatal if error in getting eth client
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -67,7 +67,7 @@ func NewRedisClient(redisConnection string, redisConfig *config.RedisConfig) (*r
 func NewEthClient(address string) (*ethclient.Client, error) {
 	ethClient, err := ethclient.Dial(address)
 	if err != nil {
-		log.Error("error in getting eth client: ", err)
+		log.Fatalf("error in getting eth client: ", err)
 	}
 
 	return ethClient, nil
