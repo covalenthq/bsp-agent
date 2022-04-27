310704b506949b683d6973a5d52a4e7babb80185
Pranay Valson <pranay.valson@gmail.com>
2022-04-27 10:28:06 -0700

    increases proof tx timeout to 180 seconds
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- internal/proof/proof.go
+++ internal/proof/proof.go
@@ -19,7 +19,7 @@ import (
 )
 
 const (
-	proofTxTimeout uint64 = 60
+	proofTxTimeout uint64 = 180
 )
 
 // SendBlockReplicaProofTx calls the proof-chain contract to make a transaction for the block-replica that it is processing
