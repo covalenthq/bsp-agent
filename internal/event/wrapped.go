a94be9b31d66ed2edc29b0ca294770d134c052d9
Pranay Valson <pranay.valson@gmail.com>
2022-05-17 13:25:43 -0700

    updates event package; removes wrapped.go
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- internal/event/wrapped.go
+++
@@ -1,9 +0,0 @@
-package event
-
-// ReplicaSegmentWrapped wraps a ReplicationSegment with additional information
-type ReplicaSegmentWrapped struct {
-	ReplicationSegment
-	IDBatch     []string
-	SkipIDBatch []string
-	SegmentName string
-}
