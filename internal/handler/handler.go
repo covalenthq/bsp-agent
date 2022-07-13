8a216146769035351260c89f3958fc94d3ffb2af
Pranay Valson <pranay.valson@gmail.com>
2022-07-13 12:09:48 -0700

    replaces ubiq rlp library with ethereum; applies replica mod
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,14 +2,13 @@
 package handler
 
 import (
-	"bytes"
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/go-redis/redis/v7"
 	"github.com/golang/snappy"
 	"github.com/linkedin/goavro/v2"
 	log "github.com/sirupsen/logrus"
-	"github.com/ubiq/go-ubiq/rlp"
 
 	"github.com/covalenthq/bsp-agent/internal/event"
 	"github.com/covalenthq/bsp-agent/internal/types"
@@ -22,8 +21,10 @@ func EncodeReplicaSegmentToAvro(replicaAvro *goavro.Codec, blockReplicaSegment i
 	if err != nil {
 		return nil, fmt.Errorf("error in converting struct to map: %w", err)
 	}
+	// Convert native <nil> to goavro Union type expected by codec
+	replicaMod := utils.MapToAvroUnion(replicaMap)
 	// Convert native Go map[string]interface{} to binary Avro data
-	binaryReplicaSegment, err := replicaAvro.BinaryFromNative(nil, replicaMap)
+	binaryReplicaSegment, err := replicaAvro.BinaryFromNative(nil, replicaMod)
 	if err != nil {
 		log.Error("failed to convert Go map to Avro binary data: ", err)
 	}
@@ -56,7 +57,8 @@ func ParseMessageToBlockReplica(msg redis.XMessage) (*event.BlockReplicaEvent, e
 	}
 
 	var blockReplica types.BlockReplica
-	err = rlp.Decode(bytes.NewReader(decodedData), &blockReplica)
+
+	err = rlp.DecodeBytes(decodedData, &blockReplica)
 	if err != nil {
 		log.Error("error decoding RLP bytes to block-replica: ", err)
 
