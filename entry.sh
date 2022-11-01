8abeed5790e23e731dbeb5daf20b8f274fe71379
Pranay Valson <pranay.valson@gmail.com>
2022-10-31 22:23:54 -0700

    update docker cirun for data in replication-2 topic
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$BLOCKCHAIN" == "elrond" ]
 then
-    timeout 120s ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-1#replicate \
+    timeout 120s ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-2#replicate \
     --avro-codec-path=./codec/block-elrond.avsc \
     --binary-file-path=./bin/block-elrond/ \
     --proof-chain-address=0xea2ff902dbeeeccc828757b881b343f9316752e5\
@@ -9,7 +9,7 @@ then
     --websocket-urls="34.69.250.147:20000 34.69.250.147:20001 34.69.250.147:20002 34.69.250.147:20003"
 
 else
-    ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-1#replicate  \
+    ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-2#replicate  \
     --avro-codec-path=./codec/block-ethereum.avsc \
     --binary-file-path=./bin/block-ethereum/ \
     --block-divisor=3 \
