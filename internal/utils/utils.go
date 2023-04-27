c2dc52fe834d2baa21093ea02ba7948b5763917e
Pranay Valson <pranay.valson@gmail.com>
2023-04-27 14:51:52 -0700

    update version to 1.4.7
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -38,7 +38,7 @@ const (
 	// BspAgentVersionMinor is Minor version component of the current release
 	BspAgentVersionMinor = 4
 	// BspAgentVersionPatch is Patch version component of the current release
-	BspAgentVersionPatch = 6
+	BspAgentVersionPatch = 7
 )
 
 // BspAgentVersion holds the textual version string.
