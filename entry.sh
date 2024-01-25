29791efee580e079b41033455c5f7f5d66b25208
Pranay Valson <pranay.valson@gmail.com>
2024-01-25 12:21:34 -0800

    update docker deploy scripts
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- entry.sh
+++ entry.sh
@@ -4,16 +4,17 @@ then
     timeout 120s ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-2#replicate \
     --avro-codec-path=./codec/block-elrond.avsc \
     --binary-file-path=./bin/block-elrond/ \
-    --proof-chain-address=0xea2ff902dbeeeccc828757b881b343f9316752e5\
+    --proof-chain-address=0xaace2c5d9c3eef552ed82422a81aa7fee90b0c4a\
     --consumer-timeout=15 \
     --websocket-urls="34.69.250.147:20000 34.69.250.147:20001 34.69.250.147:20002 34.69.250.147:20003"
 
 else
-    ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication#replicate  \
-    --avro-codec-path=./codec/block-ethereum.avsc \
-    --binary-file-path=./bin/block-ethereum/ \
-    --block-divisor=3 \
-    --proof-chain-address=0xea2ff902dbeeeccc828757b881b343f9316752e5 \
-    --consumer-timeout=15 \
+    ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-2#replicate \
+    --avro-codec-path=./codec/block-ethereum.avsc \ 
+    --binary-file-path=./bin/block-ethereum/  \
+    --block-divisor=3  \
+    --log-folder ./logs/ \ 
+    --metrics --metrics.port 6063 --metrics.addr 0.0.0.0 \ 
+    --consumer-timeout=100000 \
     --ipfs-pinner-server="http://ipfs-pinner:3001/"
 fi
