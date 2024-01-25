e55200ab070a72f231ca2e7e11248baa922e35a0
Pranay Valson <pranay.valson@gmail.com>
2024-01-25 13:09:39 -0800

    make calls from agent container explicit
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- entry.sh
+++ entry.sh
@@ -4,7 +4,7 @@ then
     timeout 120s ./bsp-agent --redis-url=redis://username:@redis:6379/0?topic=replication-2#replicate \
     --avro-codec-path=./codec/block-elrond.avsc \
     --binary-file-path=./bin/block-elrond/ \
-    --proof-chain-address=0xaace2c5d9c3eef552ed82422a81aa7fee90b0c4a\
+    --proof-chain-address=0xEa2ff902dbeEECcc828757B881b343F9316752e5\
     --consumer-timeout=15 \
     --websocket-urls="34.69.250.147:20000 34.69.250.147:20001 34.69.250.147:20002 34.69.250.147:20003"
 
@@ -14,7 +14,8 @@ else
     --binary-file-path=./bin/block-ethereum/  \
     --block-divisor=3  \
     --log-folder ./logs/ \ 
+    --proof-chain-address=0xEa2ff902dbeEECcc828757B881b343F9316752e5 \
     --metrics --metrics.port 6063 --metrics.addr 0.0.0.0 \ 
-    --consumer-timeout=100000 \
+    --consumer-timeout=15 \
     --ipfs-pinner-server="http://ipfs-pinner:3001/"
 fi
