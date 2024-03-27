f78acefea7d9215b3cb59e8109a53f6b2d6f01c7
Pranay Valson <pranay.valson@gmail.com>
2024-03-26 18:20:05 -0700

    update codec
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -241,10 +241,11 @@ func UnwrapAvroUnion(data map[string]interface{}) map[string]interface{} {
 	unwrapType(data, blobFeeCapLens, "bytes")
 	unwrapType(data, blobHashesLens, "array")
 	unwrapType(data, blobGasLens, "int")
-	unwrapType(data, blobTxSidecarLens, "array")
-	// unwrapType(data, blobsLens, "string")
-	// unwrapType(data, commitmentsLens, "string")
-	// unwrapType(data, proofsLens, "string")
+	
+	unwrapType(data, blobTxSidecarLens, "record")
+	unwrapType(data, blobsLens, "string")
+	unwrapType(data, commitmentsLens, "string")
+	unwrapType(data, proofsLens, "string")
 
 	return data
 }
@@ -278,10 +279,11 @@ func MapToAvroUnion(data map[string]interface{}) map[string]interface{} {
 	wrapType(data, blobFeeCapLens, "bytes")
 	wrapType(data, blobHashesLens, "array")
 	wrapType(data, blobGasLens, "int")
-	wrapType(data, blobTxSidecarLens, "array")
-	// wrapType(data, blobsLens, "string")
-	// wrapType(data, commitmentsLens, "string")
-	// wrapType(data, proofsLens, "string")
+
+	wrapType(data, blobTxSidecarLens, "record")
+	wrapType(data, blobsLens, "string")
+	wrapType(data, commitmentsLens, "string")
+	wrapType(data, proofsLens, "string")
 
 	return data
 }
